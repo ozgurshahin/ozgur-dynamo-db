548bc31b55b53504c1df67c26ba4b1e713c466d3
Ozgur Demir <ozgur.demir@freelancer.run>
2022-08-02 13:40:49 +0100

    UpdateConfig

--- src/main/java/com/example/ozgurdynamodb/configurations/DynamoDBConfig.java
+++ src/main/java/com/example/ozgurdynamodb/configurations/DynamoDBConfig.java
@@ -5,11 +5,15 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverterFactory;
 import com.example.ozgurdynamodb.repositories.TestLogsRepository;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @EnableDynamoDBRepositories(basePackageClasses = TestLogsRepository.class)
@@ -34,5 +38,29 @@ public class DynamoDBConfig {
                 .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey)))
                 .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(amazonDynamoDBEndpoint, amazonDynamoDBSigningRegion))
                 .build();
+    }
+
+    @Bean
+    @Primary
+    public DynamoDBMapperConfig dynamoDBMapperConfig() {
+        DynamoDBMapperConfig.Builder builder = new DynamoDBMapperConfig.Builder();
+        builder.withTypeConverterFactory(DynamoDBTypeConverterFactory.standard());
+        builder.withTableNameResolver(DynamoDBMapperConfig.DefaultTableNameResolver.INSTANCE);
+        builder.withTableNameOverride(tableNameOverrider());
+        return builder.build();
+    }
+
+    @Bean
+    @Primary
+    public DynamoDBMapperConfig.TableNameOverride tableNameOverrider() {
+        String prefix = "HERE_CAN_BE_ENV_NAME" + "_";
+        return DynamoDBMapperConfig.TableNameOverride.withTableNamePrefix(prefix);
+
+    }
+
+    @Bean
+    @Primary
+    public DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB, DynamoDBMapperConfig dynamoDBMapperConfig) {
+        return new DynamoDBMapper(amazonDynamoDB, dynamoDBMapperConfig);
     }
 }
